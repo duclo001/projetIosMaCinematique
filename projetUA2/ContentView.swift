a486d9af506970b40d3d939a0b9ed77db2eb6f45
duclo001 <duclonawildguins23@gmail.com>
2026-07-11 15:47:09 -0400

    Initial Commit

---
+++ projetUA2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projetUA2
+//
+//  Created by User on 2026-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projetUA2/projetUA2App.swift
@@ -0,0 +1,17 @@
+//
+//  projetUA2App.swift
+//  projetUA2
+//
+//  Created by User on 2026-07-11.
+//
+
+import SwiftUI
+
+@main
+struct projetUA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
